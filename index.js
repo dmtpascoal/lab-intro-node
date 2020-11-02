db4b74aefdf21c33f44c03274b0c479967df923a
Diogo Pascoal <dmtpascoal@gmail.com>
2020-11-02 15:50:22 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b
+    });
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos <= this.items.length && pos >= 0) {
+      return this.items.indexOf(pos);
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  min() {}
+  sum() {
+    let sum = this.items.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+    return sum;
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return (this.sum()) / (this.items.length);
+    };
+  }
 }
-
-module.exports = SortedList;
+  module.exports = SortedList;
